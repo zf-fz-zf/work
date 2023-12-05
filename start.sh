1d8e7fd5ab48f10dcd4bd8d3b7560bf115ffb9e4
zf-fz-zf <152966277+zf-fz-zf@users.noreply.github.com>
2023-12-05 12:31:50 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -18,7 +18,7 @@ EXTENSIONS=(
 #   "https://github.com/hako-mikan/sd-webui-regional-prompter"
     "https://github.com/Coyote-A/ultimate-upscale-for-automatic1111"
 #   "https://github.com/fkunn1326/openpose-editor"
-    "https://github.com/Gourieff/sd-webui-reactor"
+#    "https://github.com/Gourieff/sd-webui-reactor"
 )
 
 CHECKPOINT_MODELS=(
